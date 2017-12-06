f57cf00e75b337d29055d4f0db0d70de3bb0fee3
15foleyj <jfoley9@myvu.vinu.edu>
2017-12-06 20:01:31 +0000

    Updated RGBSensor File, and added RGB2. If using this code for use, start with RGB sensor. Use RGB2 if you want to make changes yourself, as RGB2 is a testing file. I have RGBSensor, as I file I can always relate back to if a problem occurs. This system is not yet fully function and lags behind when producing the correct color. It seems to have a pattern after reading 2 or 3 times, then allows color to be read again. If you can find any missing information please feel free to commit your own changes and send it back.
    Sincerely, a very stressed college student

--- RGB2.py
+++ RGB2.py
@@ -1,16 +1,33 @@
+# -*- coding: utf-8 -*-
 #working
 import time
 import RPi.GPIO as IO
 IO.setmode (IO.BCM)
 IO.setwarnings(False)  #do not show any warnings
+
+import socket
+SERVERIP = '10.0.0.52'
+n = 0
+while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((SERVERIP, 8881))
+    print "%d : Connected to server" %n
+    data = " 'Josie', 'INDX', '12345.67"
+    sock.sendall(data)
+    print "Sent", data
+    sock.close()
+    n +=1
+    time.sleep(30)
+
 x=0
-IO.setup(6,IO.OUT)     #pins 6,5,… are set as output
+IO.setup(6,IO.OUT)      #pins 6,5,… are set as output
 IO.setup(5,IO.OUT)
 IO.setup(27,IO.OUT)
 IO.setup(17,IO.OUT)
 IO.setup(13,IO.OUT)
 IO.setup(22,IO.OUT)
 IO.setup(26,IO.OUT)
+
 IO.setup(21,IO.IN)     #pins 21,20… are set as input
 IO.setup(20,IO.IN)
 IO.setup(16,IO.IN)
@@ -19,40 +36,33 @@ IO.setup(25,IO.IN)
 IO.setup(24,IO.IN)
 IO.setup(23,IO.IN)
 IO.setup(18,IO.IN)
+
 while 1:
     IO.output(6,0)     #choose red array by putting S2 and S3 low
     IO.output(5,0)
-    time.sleep(0.01)
+    time.sleep(0.05)
     IO.output(26,1)    #reset counter one time
-    time.sleep(0.02)
+    time.sleep(0.05)
     IO.output(26,0)
     IO.output(22,0)    #enable output of module for 100msec for counter to read frequency
-    time.sleep(0.01)
+    time.sleep(0.05)
     IO.output(22,1)
     if(IO.input(21)==True):
         x=1
-        print "21 is high"
     if(IO.input(20)==True):
         x=x+2
-        print "20 is high"
     if(IO.input(16)==True):
         x=x+4
-        print "16 is high"
     if(IO.input(12)==True):
         x=x+8
-        print "12 is high"
     if(IO.input(25)==True):
         x=x+16
-        print "25 is high"
     if(IO.input(24)==True):
         x=x+32
-        print "24 is high"
     if(IO.input(23)==True):
         x=x+64
-        print "23 is high"
     if(IO.input(18)==True):
         x=x+128
-        print "18 is high"
     print("Red=")     #detect value counted by counter
     R = x-50
     print(R)
@@ -60,12 +70,12 @@ while 1:
     
     IO.output(6,0)    #choose blue array
     IO.output(5,1)
-    time.sleep(0.01)
+    time.sleep(0.05)
     IO.output(26,1)   #reset counter one time
-    time.sleep(0.02)
+    time.sleep(0.05)
     IO.output(26,0)
     IO.output(22,0)   #enable output of module for 100msec for counter to read frequency
-    time.sleep(0.1)
+    time.sleep(0.05)
     IO.output(22,1)
     if(IO.input(21)==True):
         x=1
@@ -90,12 +100,12 @@ while 1:
     
     IO.output(6,1)    #choose green array
     IO.output(5,1)
-    time.sleep(0.01)
+    time.sleep(0.05)
     IO.output(26,1)   #reset counter one time
-    time.sleep(0.02)
+    time.sleep(0.05)
     IO.output(26,0) 
     IO.output(22,0)   #enable output of module for 100msec for counter to read frequency
-    time.sleep(0.01)
+    time.sleep(0.05)
     IO.output(22,1)
     if(IO.input(21)==True):
         x=1
@@ -123,6 +133,7 @@ while 1:
     print
     print
     print
+
     if((R>=B+10)and(R>=G+10)):    #if RED color intensity is high light RED led
         IO.output(17,0)
     elif((G>=B+10)and(G>=R+10)):  #if GREEN color intensity is high light GREEN led
@@ -131,7 +142,8 @@ while 1:
         IO.output(27,0)
     
     
-    time.sleep(4)    #after 2 sec turn off LEDs
+    time.sleep(2)    #after 2 sec turn off LEDs
     IO.output(17,1)
     IO.output(13,1)
     IO.output(27,1)
+
